e872fc3c01d6e964d57fe8f12023fc851cf4db8d
nunyvega <nunyvega@hotmail.com>
2023-10-25 18:48:15 +0200

    my brain hurts

--- src/main.rs
+++ src/main.rs
@@ -41,4 +41,66 @@ fn main() {
     let earth = &&&&planet;
 
     assert_eq!("EARTH", earth.to_uppercase());
+
+    let starship: Option<String> = Some("Omaha".to_string());
+
+    match starship {
+        //The ref keyword is used here to take a reference of the value inside Some rather than taking ownership of the value.
+        // This way, starship retains ownership of its value.
+        Some(ref name) => println!("{}", name),
+        None => {}
+    }
+
+    // Without the use of the `ref` keyword above, this next line would not compile:
+    println!("{:?}", starship);
+    // {:?} is used for formatting, which means it will print the debug representation of starship.
+    println!("{:#?}", starship);
+    // {:#?} also exists for pretty print
+
+    let val = "reciprocal";
+
+    let ref r1 = val;
+    let r2 = &val;
+
+    assert_eq!(r1, r2);
+
+    let s = String::from("hello world");
+
+    let hello = &s[0..5]; // Slices are a reference to a range of elements in a collection
+    let world = &s[6..11];
+    println!("{hello}{world}");
+
+    // This is a closure
+    let double = |d| d * 2;
+
+    // This is the outcome of calling the closure
+    let var = double(10);
+
+    // This will re-assign the value of var
+    let doubled_var = var;
+
+    println!("{}", doubled_var);
+
+    //Shadowing an inmutable variable and making it mutable
+    let number = 10;
+    let mut number = number + 10;
+    number += 10;
+    println!("{number}"); // prints "23"
+
+    fn question(s: &mut String) {
+        s.pop();
+        s.push('?');
+    }
+
+    let mut sentence = String::from("I am.");
+    question(&mut sentence);
+
+    println!("{sentence}");
+
+    let mut sentence = String::from("Take care, take care.");
+    let immutable_reference = &mut sentence;
+
+    // Swapping the order of these statements will cause our code to not compile.
+    println!("{}", immutable_reference);
+    println!("{}", sentence);
 }
